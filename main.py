ba61f2b3bbb536cba58430137c7d2b1b9d948632
PenutChen <penut85420@gmail.com>
2020-05-27 17:59:40 +0800

    Update main.py
    + Change URI of API

--- main.py
+++ main.py
@@ -23,43 +23,45 @@ def main():
     driver.implicitly_wait(1)
     first = True
     try:
-        for d in data:
+        for i, d in enumerate(data):
+            print('%-50s' % f'{i + 1}/{len(data)} {d[0]} => {d[1]}', end='\r')
             if d[3].lower() == 'ok':
                 continue
-            driver.get(url % d[0])
-            if first:
-                driver.find_element_by_class_name('tlid-dismiss-button').click()
-            element = driver.find_element_by_class_name('tlid-send-feedback-link')
-            driver.execute_script("arguments[0].click();", element)
-            time.sleep(1)
-            n = len(driver.find_elements_by_tag_name('iframe'))
-            print(f'{n} iframe found')
-            for i in reversed(range(n)):
-                driver.switch_to.frame(i)
-                e = driver.find_elements_by_tag_name('textarea')
-                if len(e) == 1:
-                    e[0].send_keys(template % d[1])
-                    driver.find_element_by_xpath('//input[@type="checkbox"]').click()
-                    driver.find_element_by_xpath('//button[@type="submit"]').click()
+            try:
+                driver.get(url % d[0])
+                if first:
+                    driver.find_element_by_class_name('tlid-dismiss-button').click()
+                element = driver.find_element_by_class_name('tlid-send-feedback-link')
+                driver.execute_script("arguments[0].click();", element)
+                time.sleep(1)
+                n = len(driver.find_elements_by_tag_name('iframe'))
+                for i in reversed(range(n)):
+                    driver.switch_to.frame(i)
+                    e = driver.find_elements_by_tag_name('textarea')
+                    if len(e) == 1:
+                        e[0].send_keys(template % d[1])
+                        driver.find_element_by_xpath('//input[@type="checkbox"]').click()
+                        driver.find_element_by_xpath('//button[@type="submit"]').click()
+                        driver.switch_to.default_content()
+                        break
                     driver.switch_to.default_content()
-                    print(f'Feedback submit')
-                    break
-                driver.switch_to.default_content()
 
-            time.sleep(1)
-            driver.find_element_by_class_name('tlid-suggest-edit-button').click()
-            driver.find_element_by_class_name('contribute-target').clear()
-            driver.find_element_by_class_name('contribute-target').send_keys(d[1])
-            driver.find_element_by_class_name('jfk-button-action').click()
-            print(f'Suggestion submit')
-            first = False
+                time.sleep(1)
+                driver.find_element_by_class_name('tlid-suggest-edit-button').click()
+                driver.find_element_by_class_name('contribute-target').clear()
+                driver.find_element_by_class_name('contribute-target').send_keys(d[1])
+                driver.find_element_by_class_name('jfk-button-action').click()
+                first = False
+            except Exception as e:
+                print(f'\nError, skip!')
     finally:
         driver.quit()
 
     print('Done!')
 
 def get_sheet():
-    return json.loads(requests.get('http://nlp4.cse.ntou.edu.tw:8080/').content)
+    content = requests.get('http://gtig.ddns.net/').content
+    return json.loads(content)
 
 if __name__ == "__main__":
     main()
